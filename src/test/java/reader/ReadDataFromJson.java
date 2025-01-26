413004f76e210f72c54e6cfad098d4427e3e5a28
mennaz126 <menna.eid@unioncoop.ae>
2025-01-26 14:16:07 +0200

    Contact us Form

--- src/test/java/reader/ReadDataFromJson.java
+++ src/test/java/reader/ReadDataFromJson.java
@@ -9,7 +9,7 @@ import java.io.FileReader;
 public class ReadDataFromJson {
 
     public DataModel readJsonFile() throws FileNotFoundException {
-        FileReader fileReader = new FileReader("src/test/resources/data/testData.json");
+        FileReader fileReader = new FileReader("data/testData.json");
         return new Gson().fromJson(fileReader, DataModel.class);
        // FileReader fileReader = new FileReader("data/testData.json");
 
